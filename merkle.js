1d6dfb776798c99e212d1055d886b3fba8c289a5
kofi-js <vwegbakofi@gmail.com>
2025-01-25 20:05:27 +0100

    new commit

--- merkle.js
+++ merkle.js
@@ -2,7 +2,14 @@ import { MerkleTree } from 'merkletreejs';
 import keccak256 from 'keccak256';
 
 //list of 5  addresses
-const addresses = ['addr1', 'addr2', 'addr3', 'addr4', 'addr5'];
+
+const addresses = [
+    '0x1234567890123456789012345678901234567890',
+    '0x2345678901234567890123456789012345678901',
+    '0x3456789012345678901234567890123456789012',
+    '0x4567890123456789012345678901234567890123',
+    '0x5678901234567890123456789012345678901234'
+];
 
 // Convert each address to a buffer using keccak256 hash
 const leaves = addresses.map(x => keccak256(x));
@@ -14,8 +21,8 @@ const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 const root = tree.getHexRoot();
 console.log('Merkle Root:', root);
 
-// Get proof for the first address ('addr1')
-const leaf = keccak256('addr1');
+// Get proof for the first address
+const leaf = keccak256('0x1234567890123456789012345678901234567890');
 const proof = tree.getHexProof(leaf);
 
 // Verify the proof
